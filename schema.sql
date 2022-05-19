461b3f534c3b5e39e0ad1583a12a5ce4c98126f8
child-sawko <sahakozplay@gmail.com>
2022-05-19 11:51:29 +0300

    7th-task

--- schema.sql
+++ schema.sql
@@ -1,111 +1,90 @@
--- Запрос на создание всей базы данных, со связями, и ключами.
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-
--- База данных: `bd-yeticave`
-
-CREATE DATABASE bd-yeticave;
-use bd-yeticave;
 -- --------------------------------------------------------
-
-
 -- Структура таблицы `bid`
 
-
 CREATE TABLE `bid` (
-                     `data-bid` datetime NOT NULL,
-                     `sum` int NOT NULL,
-                     `id-users` int NOT NULL,
-                     `id-bid` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
+                     `id_bid` int(11) NOT NULL,
+                     `id_lot` int(11) DEFAULT NULL,
+                     `id_user` int(11) NOT NULL,
+                     `date_bid` datetime NOT NULL,
+                     `sum` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- --------------------------------------------------------
-
-
 -- Структура таблицы `category`
 
-
 CREATE TABLE `category` (
-                          `id-category` int NOT NULL,
-                          `name` varchar(256) NOT NULL,
-                          `id-users` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
+                          `id_category` int(11) NOT NULL,
+                          `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- --------------------------------------------------------
-
 
 -- Структура таблицы `lot`
 
 
 CREATE TABLE `lot` (
-                     `data-create` date NOT NULL,
-                     `name` int NOT NULL,
-                     `disscription` varchar(512) NOT NULL,
-                     `img-url` varchar(1024) NOT NULL,
-                     `start-price` int NOT NULL,
-                     `step-bid` int NOT NULL,
-                     `data-finish` int NOT NULL,
-                     `author` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                     `winner` int NOT NULL,
-                     `id-lot` int NOT NULL,
-                     `id-category` int NOT NULL,
-                     `id-users` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
+                     `id_lot` int(11) NOT NULL,
+                     `id_winner` int(11) NOT NULL,
+                     `id_user` int(11) NOT NULL,
+                     `id_category` int(11) NOT NULL,
+                     `date_create` datetime NOT NULL,
+                     `name` varchar(250) NOT NULL,
+                     `description` varchar(1000) NOT NULL,
+                     `image_url` varchar(250) NOT NULL,
+                     `start_price` int(11) NOT NULL,
+                     `date_finish` datetime NOT NULL,
+                     `bid_step` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- --------------------------------------------------------
-
 
 -- Структура таблицы `users`
 
 
 CREATE TABLE `users` (
-                       `data-registration` int NOT NULL,
-                       `email` int NOT NULL,
-                       `username` int NOT NULL,
-                       `password` int NOT NULL,
-                       `img-avatar` blob NOT NULL,
-                       `contacts` varchar(128) NOT NULL,
-                       `id-users` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+                       `id_user` int(11) NOT NULL,
+                       `date_registration` datetime NOT NULL,
+                       `email` varchar(250) NOT NULL,
+                       `name` varchar(250) NOT NULL,
+                       `password` varchar(100) NOT NULL,
+                       `avatar` varchar(250) NOT NULL,
+                       `contacts` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- Индексы сохранённых таблиц
 
 
-
 -- Индексы таблицы `bid`
 
+
 ALTER TABLE `bid`
-  ADD PRIMARY KEY (`id-bid`),
-  ADD UNIQUE KEY `id-users` (`id-users`),
-  ADD UNIQUE KEY `sum` (`sum`);
+  ADD PRIMARY KEY (`id_bid`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_lot` (`id_lot`);
 
 
 -- Индексы таблицы `category`
 
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id-category`),
-  ADD UNIQUE KEY `id-users` (`id-users`);
+  ADD PRIMARY KEY (`id_category`);
 
 
 -- Индексы таблицы `lot`
 
 ALTER TABLE `lot`
-  ADD PRIMARY KEY (`id-lot`),
-  ADD UNIQUE KEY `winner` (`winner`),
-  ADD UNIQUE KEY `id-category` (`id-category`),
-  ADD UNIQUE KEY `id-users` (`id-users`),
-  ADD UNIQUE KEY `start-price` (`start-price`);
+  ADD PRIMARY KEY (`id_lot`,`id_winner`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_category` (`id_category`),
+  ADD KEY `id_winner` (`id_winner`);
 
 
 -- Индексы таблицы `users`
 
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id-users`);
+  ADD PRIMARY KEY (`id_user`);
 
 
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -115,41 +94,46 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `bid`
 
 ALTER TABLE `bid`
-  MODIFY `id-bid` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 
 -- AUTO_INCREMENT для таблицы `category`
 
 ALTER TABLE `category`
-  MODIFY `id-category` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 
 -- AUTO_INCREMENT для таблицы `lot`
 
+
 ALTER TABLE `lot`
-  MODIFY `id-lot` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_lot` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 
 -- AUTO_INCREMENT для таблицы `users`
 
 ALTER TABLE `users`
-  MODIFY `id-users` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 
 -- Ограничения внешнего ключа сохраненных таблиц
 
 
 
--- Ограничения внешнего ключа таблицы `category`
+-- Ограничения внешнего ключа таблицы `bid`
 
-ALTER TABLE `category`
-  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`id-category`) REFERENCES `lot` (`id-category`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `bid`
+  ADD CONSTRAINT `bid_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bid_ibfk_2` FOREIGN KEY (`id_lot`) REFERENCES `lot` (`id_lot`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- Ограничения внешнего ключа таблицы `lot`
 
--- Ограничения внешнего ключа таблицы `users`
-
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id-users`) REFERENCES `lot` (`winner`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`id-users`) REFERENCES `lot` (`id-users`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `users_ibfk_3` FOREIGN KEY (`id-users`) REFERENCES `bid` (`id-users`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `lot`
+  ADD CONSTRAINT `lot_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `lot_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `lot_ibfk_3` FOREIGN KEY (`id_winner`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
